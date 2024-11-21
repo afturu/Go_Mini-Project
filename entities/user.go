5159f2b56ace8193976819071a6c457b713364ce
Muhammad Ilhan Mansiz <milhanmansiz1512@gmail.com>
2024-11-21 15:45:01 +0700

    add preload 2

--- entities/user.go
+++ entities/user.go
@@ -12,7 +12,6 @@ type User struct {
     Email    string         `gorm:"type:varchar(100);unique;not null" json:"email"`
     Password string         `gorm:"type:varchar(255);not null" json:"password"`
     Profile  UserProfile    `gorm:"foreignKey:UserID" json:"profile"`
-    Items    []Item         `gorm:"foreignKey:UserID" json:"items"`
     CreatedAt  *time.Time    `json:"created_at,omitempty"`
     UpdatedAt  *time.Time    `json:"updated_at,omitempty"`
     DeletedAt  gorm.DeletedAt `gorm:"index" json:"-"`
