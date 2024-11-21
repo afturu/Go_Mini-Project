02260cf96206769f4f1aad780b43c81bbac9e59f
Muhammad Ilhan Mansiz <milhanmansiz1512@gmail.com>
2024-11-21 14:50:08 +0700

    update entities category

--- entities/category.go
+++ entities/category.go
@@ -3,6 +3,5 @@ package entities
 type Category struct {
     ID       uint   `gorm:"primaryKey;autoIncrement" json:"id"`
     Name     string `gorm:"type:varchar(100);not null" json:"name"`
-    ItemsID  uint   `gorm:"not null" json:"user_id"`
-    Items    []Item `gorm:"foreignKey:CategoryID" json:"items"`
+    Description string    `gorm:"type:text" json:"description"`
 }
